bf457ad4683334745f61162c6331e9fc31349ceb
Keith Schwarz <htiek@cs.stanford.edu>
2020-02-12 10:26:24 -0800

    Lecture 15, Winter 2020

--- Lecture 15/OurStack/src/Main.cpp
+++ Lecture 15/OurStack/src/Main.cpp
@@ -13,22 +13,22 @@ using namespace std;
 const int kNumElems = 4;
 
 int main() {
-	OurStack stack;
-	
-	/* Load the stack with random values. */
-  cout << "Pushing values onto the stack:" << endl;
-	for (int i = 0; i < kNumElems; i++) {
-		  int value = randomInteger(0, 1000);
-      cout << "  " << value << endl;
-		  stack.push(value);
-	}
+    OurStack stack;
 
-  cout << "Popping values from the stack: " << endl;
-	
-	/* See what those values are. */
-	while (!stack.isEmpty()) {
-      cout << "  " << stack.pop() << endl;
-	}
-	
-	return 0;
+    /* Load the stack with random values. */
+    cout << "Pushing values onto the stack:" << endl;
+    for (int i = 0; i < kNumElems; i++) {
+        int value = randomInteger(0, 1000);
+        cout << "  " << value << endl;
+        stack.push(value);
+    }
+
+    cout << "Popping values from the stack: " << endl;
+
+    /* See what those values are. */
+    while (!stack.isEmpty()) {
+        cout << "  " << stack.pop() << endl;
+    }
+
+    return 0;
 }
