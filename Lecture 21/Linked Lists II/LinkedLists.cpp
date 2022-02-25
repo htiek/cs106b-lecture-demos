4ec3ffd25b450fdc786edf7e8511899dff3b1779
Keith Schwarz <htiek@cs.stanford.edu>
2022-02-25 10:33:34 -0800

    Shift indices; L21 starter.

---
+++ Lecture 21/Linked Lists II/LinkedLists.cpp
@@ -0,0 +1,71 @@
+/*************************************************
+ * File: LinkedLists.cpp
+ *
+ * Our very second program using Linked Lists! :-D
+ */
+
+#include <iostream>
+#include <string>
+#include "simpio.h"
+#include "console.h"
+using namespace std;
+
+/* Type: Cell
+ *
+ * A cell in a linked list.
+ */
+struct Cell {
+    string value;
+    Cell* next;
+};
+
+/* Given a linked list, how long is it? */
+int lengthOf(Cell* list) {
+    /* Base case: An empty list has length zero. */
+    if (list == nullptr) return 0;
+
+    /* Recursive case: The length is one, plus the length of the
+     * remaining list.
+     */
+    return 1 + lengthOf(list->next);
+}
+
+/* From last time! */
+void printList(Cell* list) {
+    while (list != nullptr) {
+        cout << list->value << endl;
+        list = list->next;
+    }
+}
+
+/* From last time! */
+void deleteList(Cell* list) {
+    while (list != nullptr) {
+        Cell* next = list->next;
+        delete list;
+        list = next;
+    }
+}
+
+void prependTo(Cell*& list, const string& value) {
+    /* Improve upon the emptiness. */
+}
+
+int main() {
+    Cell* list = nullptr;
+
+    prependTo(list, "Elephant");
+    prependTo(list, "Sunfish");
+    prependTo(list, "Whale");
+    prependTo(list, "Piraracu");
+
+    printList(list);
+    deleteList(list);
+    return 0;
+}
+
+
+
+
+
+
