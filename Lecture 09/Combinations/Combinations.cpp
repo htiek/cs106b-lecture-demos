0ed49d4fb0471411bd96589989d1cd72625c3e9d
Keith Schwarz <htiek@cs.stanford.edu>
2025-01-28 14:36:56 -0800

    L09 Updates

--- Lecture 09/Combinations/Combinations.cpp
+++ Lecture 09/Combinations/Combinations.cpp
@@ -3,102 +3,42 @@
 #include "console.h"
 using namespace std;
 
-/*
- * { "Roberts", "Breyer", "Alito", "Thomas", "Kagan" }
- *     ^
- *     |
- *     +------ Set<string>
- *
- * {
- *   { "Roberts", "Breyer", "Alito", "Thomas", "Kagan" },
- *   { "Sotomayor", "Barrett", "Kavanaugh", "Gorsuch", "Roberts" },
- *   ...
- * } <----- Set<Set<string>>, Vector<Set<string>>
- */
-
-Set<Set<string>>
-   combinationsRec(const Set<string>& elems,
-                   int numRemaining,
-                   const Set<string>& soFar) {
-    /* Base Case: If we don't need to pick any more
-     * people, then we're done!
-     */
-    if (numRemaining == 0) {
-        return { soFar };
-    }
-    /* Base Case: If we're asked to pick more people
-     * than remain, then we can't find any solutions.
-     */
-    else if (numRemaining > elems.size()) {
-        return { };
-    }
-    /* Recursive Case: Pick a person and decide what to
-     * do with them.
-     */
-    else {
-        string elem = elems.first();
-        Set<string> remaining = elems - elem;
-
-        /* Option 1: Exclude this person. */
-        auto without = combinationsRec(remaining,
-                                       numRemaining,
-                                       soFar);
-
-        /* Option 2: Include this person. */
-        auto with = combinationsRec(remaining,
-                                    numRemaining - 1,
-                                    soFar + elem);
-
-        return with + without;
-    }
-}
-
-Set<Set<string>> combinationsOf(const Set<string>& elems,
-                                int numToPick) {
-    /* Check validity. */
-    if (numToPick < 0) {
-        error("You shouldn't ask for a negative number of people.");
-    }
-
-    return combinationsRec(elems, numToPick, {});
-}
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
 /* Current members of the US Supreme Court, ordered
- * by seniority.
+ * by seniority. (Or at least, they're ordered HERE
+ * by seniority; the Set will order them internally
+ * however it feels like it. ^_^)
+ *
+ * Fun fact: the most junior Associate Justice is
+ * responsible for, among other things, opening the
+ * door for the other justices and coordinating
+ * what's in their cafeteria.
+ *
+ * Fun fact: The Chief Justice sits, ex officio,
+ * on the Board of Regents of the Smithsonian
+ * Institution. Job perks!
  */
 const Set<string> usSupremeCourt = {
     "Roberts",
     "Thomas",
-    "Breyer",
     "Alito",
     "Sotomayor",
     "Kagan",
     "Gorsuch",
     "Kavanaugh",
-    "Barrett"
+    "Barrett",
+    "Jackson"
 };
 
 int main() {
-    auto combinations = combinationsOf(usSupremeCourt, 5);
+    Set<Set<string>> combinations = combinationsOf(usSupremeCourt, 5);
 
     cout << "There are " << combinations.size()
          << " combinations. They are: " << endl;
 
-    for (auto combination: combinations) {
+    for (Set<string> combination: combinations) {
         cout << combination << endl;
     }
     return 0;
