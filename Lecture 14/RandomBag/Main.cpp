bc3cc3fa22c55e4922255bee054ff4e9c2064961
Keith Schwarz <htiek@cs.stanford.edu>
2025-02-06 10:45:33 -0800

    L14 2025 Updates

--- Lecture 14/RandomBag/Main.cpp
+++ Lecture 14/RandomBag/Main.cpp
@@ -1,19 +1,8 @@
 #include <iostream>
 #include "console.h"
-#include "RandomBag.h"
 using namespace std;
 
 int main() {
-    /* Some sample code using the RandomBag. */
-    RandomBag bag;
-
-    for (int i = 0; i < 10; i++) {
-        bag.add(i);
-    }
-
-    for (int i = 0; i < 10; i++) {
-        cout << bag.removeRandom() << " ";
-    }
 
     return 0;
 }
